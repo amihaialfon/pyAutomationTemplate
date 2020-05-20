607d7caa948745c3b57d8fe1954809aafc6d2f8e
amihaialfonCV <amihaia@codevalue.net>
2020-05-20 15:09:25 +0300

    optimization updates

--- LimitedBeTests.py
+++ LimitedBeTests.py
@@ -16,26 +16,28 @@ scenario_id = 'NotExistingID -'
 
 def test_post_result_evaluation_21395(scenario_name):
     server.run_server()
+    start_time = datetime.now()
     logger.info('This is a test that checks post evaluation at limited BE .' + scenario_name)
     scenario = ScenarioManager.get_scenario_file(scenario_name=scenario_name)
-    start_time = datetime.now()
     r = adapter.send_evaluation(scenario, method='post')
+
     assert r is not None, 'If r is None it means that no response where received from UUT'
     response_json = adapter.convert_to_dict(r.text)
     assert Finders.check_response(str(r)), 'Responses are not correct'  # Checks response for valid range.
     current_id = {'id': response_json['id']}
+
     r2 = adapter.send_evaluation(scenario, method='post')
     assert Finders.check_response(str(r2)), 'Response is valid range'  # Checks response for valid range.
     assert Finders.find_id_in_scenario_get(response_json['id'],
                                            r2.text), 'Scenario is not among returned scenarios from UUT'
     print('Scenario is included in returned scenarios')
-    resonse = None
-    while resonse is None:
+    response = None
+    while response is None:
         print('waiting evaluation unit response' + datetime.now())
-        resonse = server.incoming_data
-    print(resonse)
+        response = server.incoming_data
+    print(response)
     server.stop_server()
-    assert resonse == '123', "the data is wrong"
+    assert response == '123', "the data is wrong"
 
 # @TODO add wait from EVAL unit post message
 
